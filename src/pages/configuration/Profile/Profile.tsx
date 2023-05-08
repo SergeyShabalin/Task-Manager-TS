b0a34b1b51c67e7dd693fdb7e263c3eda7cc0d53
ssworkss <obaldevshiy@yandex.ru>
2023-05-08 21:15:15 +0300

    profile css

--- src/pages/configuration/Profile/Profile.tsx
+++ src/pages/configuration/Profile/Profile.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import classes from './Profile.module.css'
 
 export default function Profile() {
 	return (
-		<div>
-			Настройки профиля
+		<div className={classes.wrapper}>
+			<h1>Настройки профиля</h1>
+			<div className={classes.content}>
+				<span>Фото профиля и изображение обложки</span>
+				<div className={classes.wrapper_photo}>
+					<div className={classes.background}></div>
+					<div className={classes.avatar}></div>
+					<div className={classes.white_background}></div>
+				</div>
+			</div>
+
 		</div>
 	)
 }
