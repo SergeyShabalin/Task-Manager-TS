22af92aa208e8ddf4d344547faa1ec6d6bc85655
ssworkss <obaldevshiy@yandex.ru>
2023-05-09 22:11:10 +0300

    profile css bit

--- src/pages/configuration/Profile/Profile.tsx
+++ src/pages/configuration/Profile/Profile.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './Profile.module.css'
+import { AiOutlineCloudUpload } from 'react-icons/all'
 
 export default function Profile() {
+
+	const inputRef = useRef<HTMLInputElement>(null)
+	const [image, setImage] = useState('')
+
+	function handleImageClick(){
+		inputRef.current?.click()
+	}
+	function handleImageChange(e){
+		const file = e.target.files[0]
+		console.log(file)
+	 // setImage()
+	}
+
 	return (
 		<div className={classes.wrapper}>
 			<h1>Настройки профиля</h1>
 			<div className={classes.content}>
-				<span>Фото профиля и изображение обложки</span>
+				<div className={classes.title_photo}>Фото профиля и изображение обложки</div>
 				<div className={classes.wrapper_photo}>
-					<div className={classes.background}></div>
+					<div className={classes.background} onClick={handleImageClick}></div>
+					<div className={classes.background_shadow} >
+						<div>Загрузить новое фото </div>
+					<span><AiOutlineCloudUpload/></span>
+					</div>
+					<input hidden type={'file'} ref={inputRef} onChange={handleImageChange}/>
 					<div className={classes.avatar}></div>
 					<div className={classes.white_background}></div>
 				</div>
