6e3e7e04b64e1d5963f2b67066cf9dc43a36c3b2
ssworkss <obaldevshiy@yandex.ru>
2023-05-10 14:53:06 +0300

    profile avatar css

--- src/pages/configuration/Profile/Profile.tsx
+++ src/pages/configuration/Profile/Profile.tsx
@@ -1,36 +1,52 @@
 import React, { useRef, useState } from 'react'
 import classes from './Profile.module.css'
-import { AiOutlineCloudUpload } from 'react-icons/all'
+import { AiOutlinePicture, MdAddAPhoto } from 'react-icons/all'
 
 export default function Profile() {
 
 	const inputRef = useRef<HTMLInputElement>(null)
+	const inputAvatarRef = useRef<HTMLInputElement>(null)
 	const [image, setImage] = useState('')
 
 	function handleImageClick(){
 		inputRef.current?.click()
 	}
+	function handleImageAvatarClick(){
+		inputAvatarRef.current?.click()
+	}
 	function handleImageChange(e){
 		const file = e.target.files[0]
 		console.log(file)
-	 // setImage()
+	}
+	function handleImageAvatarChange(e){
+		const file = e.target.files[0]
+		console.log(file)
 	}
 
 	return (
 		<div className={classes.wrapper}>
-			<h1>Настройки профиля</h1>
+			<h1 className={classes.title_profile}>Настройки профиля</h1>
 			<div className={classes.content}>
 				<div className={classes.title_photo}>Фото профиля и изображение обложки</div>
 				<div className={classes.wrapper_photo}>
-					<div className={classes.background} onClick={handleImageClick}></div>
-					<div className={classes.background_shadow} >
-						<div>Загрузить новое фото </div>
-					<span><AiOutlineCloudUpload/></span>
+					<div className={classes.background} onClick={handleImageClick}>
+						<div className={classes.background_shadow} >
+							<div className={classes.download_title}>Загрузить новое фото </div>
+							<span className={classes.download_logo}><AiOutlinePicture/></span>
+						</div>
 					</div>
+
 					<input hidden type={'file'} ref={inputRef} onChange={handleImageChange}/>
-					<div className={classes.avatar}></div>
+					<input hidden type={'file'} ref={inputAvatarRef} onChange={handleImageAvatarChange}/>
+					<div className={classes.avatar} onClick={handleImageAvatarClick}>
+						<div className={classes.avatar_shadow}>
+						<span className={classes.logo_avatar}><MdAddAPhoto/></span>
+						</div>
+					</div>
 					<div className={classes.white_background}></div>
 				</div>
+
+				<div className={classes.title_photo}>Информация о пользователе</div>
 			</div>
 
 		</div>
