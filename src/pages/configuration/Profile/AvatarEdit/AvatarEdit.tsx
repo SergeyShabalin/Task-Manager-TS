9f0d612e668c689320b0ade41595b7dc8f5220e3
ssworkss <obaldevshiy@yandex.ru>
2023-05-11 16:56:50 +0300

    change avatar server, redux

--- src/pages/configuration/Profile/AvatarEdit/AvatarEdit.tsx
+++ src/pages/configuration/Profile/AvatarEdit/AvatarEdit.tsx
@@ -1,35 +1,79 @@
 import React, { useState } from 'react'
 import Avatar from 'react-avatar-edit'
-import { Button } from '@UI'
+import { Button, Modal } from '@UI'
+import classes from './AvatarEdit.module.css'
+import { useTypedSelector } from '@/hooks/useTypedSelector/useTypedSelector'
+import { useNavigate } from 'react-router-dom'
+import { useActions } from '@/hooks/useActions/useActions'
+
+export default function AvatarEdit() {
+
+	const user = useTypedSelector(state => state.user)
+	const navigate = useNavigate()
+	const [preview, setPreview] = useState('')
+	const { changeUser } = useActions()
 
 
-export default function AvatarEdit({showw}) {
-
-	const [src, setSrc] = useState(null)
-	const [preview, setPreview] = useState(null)
 
 	function closePhoto() {
-		setPreview(null)
+		setPreview('')
 	}
 
-	function cropPhoto(photo) {
+	function cropPhoto(photo: string) {
 		setPreview(photo)
 	}
 
+	function closeModal() {
+		navigate(`/user/${user._id}/configuration/profile`)
+	}
 
+	function downloadPhoto(){
+		const payload = {
+			_id: user._id,
+			avatar: preview
+		}
+	   changeUser(payload)
+		 closeModal()
+
+	}
 
 	return (
-		<div>
-			<Avatar
-				width={400}
-				height={400}
-				onClose={closePhoto}
-				onCrop={cropPhoto}
-			/>
+		<Modal onClose={closeModal} open>
+			<div className={classes.wrapper}>
 
-			{preview && <img src={preview} />}
+				<h1 className={classes.title}>Редактирование фото</h1>
+				<span className={classes.title_info}>Загрузите свое изображение в формате "jpg" или "jpeg"</span>
 
-			<Button title={'показать'} variant={'outlined'} onClick={()=>showw(preview)}/>
-		</div>
+				<div className={classes.select_avatar}>
+					<Avatar
+						width={300}
+						height={300}
+						onClose={closePhoto}
+						onCrop={cropPhoto}
+					/>
+				</div>
+
+				<div className={classes.preview_wrapper}>
+					{preview ?
+						<>
+							<div className={classes.preview}>
+								<img src={preview} />
+							</div>
+							<div className={classes.person_info}>
+								<div className={classes.name}>{user.secondName + ' ' + user.firstName}</div>
+								<span className={classes.email}>{user.email}</span>
+							</div>
+						</>
+						: <div className={classes.appearance}>Изображение в миниатюре</div>
+					}
+				</div>
+				<div className={classes.control}>
+					<div className={classes.control_btn}>
+						{preview &&	<Button title='Загрузить' color='primary' variant='contained' onClick={downloadPhoto} />}
+						<Button title='Отменить' variant='outlined' onClick={closeModal} />
+					</div>
+				</div>
+			</div>
+		</Modal>
 	)
 }
--- src/pages/configuration/Profile/Profile.tsx
+++ src/pages/configuration/Profile/Profile.tsx
@@ -1,64 +1,88 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import classes from './Profile.module.css'
 import { AiOutlinePicture, MdAddAPhoto } from 'react-icons/all'
-import AvatarEdit from '@/pages/configuration/Profile/AvatarEdit'
+import { Link, useLocation } from 'react-router-dom'
+import { useTypedSelector } from '@/hooks/useTypedSelector/useTypedSelector'
 
-export default function Profile() {
+interface ProfileProps {
+	userId: string
+}
+
+export default function Profile({ userId }: ProfileProps) {
 
 	const inputRef = useRef<HTMLInputElement>(null)
 	const inputAvatarRef = useRef<HTMLInputElement>(null)
 	const [image, setImage] = useState()
-	const [avatar, setAvatar] = useState(null)
+	const location = useLocation()
+	const { avatar, firstName, secondName } = useTypedSelector(state => state.user)
 
-
-	function handleImageClick(){
+	function handleImageClick() {
 		inputRef.current?.click()
 	}
-	function handleImageAvatarClick(){
+
+	function handleImageAvatarClick() {
 		inputAvatarRef.current?.click()
 	}
-	function handleImageChange(e){
+
+//TOdo поправить any
+	function handleImageChange(e: any) {
 		const file = e.target.files[0]
 		console.log(file)
 		setImage(file)
 	}
-	function handleImageAvatarChange(e){
+
+	function handleImageAvatarChange(e: any) {
 		const file = e.target.files[0]
 		console.log(file)
 	}
 
-	function showw(preview){
-		console.log(typeof (preview))
-	}
 
 	return (
 		<div className={classes.wrapper}>
 			<h1 className={classes.title_profile}>Настройки профиля</h1>
 			<div className={classes.content}>
 				<div className={classes.title_photo}>Фото профиля и изображение обложки</div>
+
 				<div className={classes.wrapper_photo}>
 					<div className={classes.background} onClick={handleImageClick}>
-						{image && <img src={URL.createObjectURL(image)}/>}
-						<div className={classes.background_shadow} >
-							<div className={classes.download_title}>Загрузить новое фото </div>
-							<span className={classes.download_logo}><AiOutlinePicture/></span>
+						{image && <img src={URL.createObjectURL(image)} />}
+						<div className={classes.background_shadow}>
+							<div className={classes.download_title}>Загрузить новое фото</div>
+							<span className={classes.download_logo}><AiOutlinePicture /></span>
 						</div>
 					</div>
 
-					<input hidden type={'file'} ref={inputRef} onChange={handleImageChange}/>
-					<input hidden type={'file'} ref={inputAvatarRef} onChange={handleImageAvatarChange}/>
-					<div className={classes.avatar} onClick={handleImageAvatarClick}>
-						{avatar && <img src={avatar}/>}
+					<input hidden type={'file'} ref={inputRef} onChange={handleImageChange} />
+					<input hidden type={'file'} ref={inputAvatarRef} onChange={handleImageAvatarChange} />
+					<div className={classes.avatar}>
+						{!avatar ?
+							<img className={classes.img}
+									 src={'https://avatars.mds.yandex.net/i?id=24affb1a8027ca093f1eed6f1bd35ef10e6760b1-8340947-images-thumbs&n=13'} />
+							: <img className={classes.img} src={avatar} />
+						}
 						<div className={classes.avatar_shadow}>
-						<span className={classes.logo_avatar}><MdAddAPhoto/></span>
+							<Link
+								state={{ background: location }}
+								to={`/user/${userId}/configuration/avatarEdit`}
+							>
+								<div className={classes.logo_avatar}><MdAddAPhoto /></div>
+							</Link>
+						</div>
+
+					</div>
+
+					<div className={classes.white_background}>
+						<div className={classes.person_name}>
+							<h1 className={classes.second_name}>{secondName}</h1>
+							<h1 className={classes.first_name}>{firstName}</h1>
 						</div>
 					</div>
-					<div className={classes.white_background}></div>
 				</div>
 
 				<div className={classes.title_photo}>Информация о пользователе</div>
-
-				<AvatarEdit showw={showw}/>
+				<div className={classes.person_wrapper}>
+				информация
+				</div>
 			</div>
 
 		</div>
